42b7e19af14fd91a95c3f34fb1f6079ff8556594
Aryan Jangam <aryanjangam276@gmail.com>
2024-03-02 15:10:06 +0530

    firstcommit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,25 @@ import { NavLink } from 'react-router-dom'
 function Navbar(props) {
   return (
     <>
-    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-  <div className="container-fluid">
-    <NavLink className="navbar-brand" to="/"><h2>{props.title}</h2></NavLink>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page" to="/">Converter</NavLink>
-        </li>
-      </ul>
-      <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-        <input className="form-check-input" style={{backgroundColor : props.mode==='light'?'white':'black', borderColor : props.mode==='light'?'black':'white ', boxShadow : 'none'}} type="checkbox" onClick={props.toggleMode} id="flexSwitchCheckDefault"/>
-        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.mode==='light'?'Dark':'Light'}Mode</label>
-      </div>
-    </div>
-  </div>
-</nav>
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/"><h2>{props.title}</h2></NavLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">Converter</NavLink>
+              </li>
+            </ul>
+            <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+              <input className="form-check-input" style={{ backgroundColor: props.mode === 'light' ? 'white' : 'black', borderColor: props.mode === 'light' ? 'black' : 'white ', boxShadow: 'none' }} type="checkbox" onClick={props.toggleMode} id="flexSwitchCheckDefault" />
+              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.mode === 'light' ? 'Dark' : 'Light'}Mode</label>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
